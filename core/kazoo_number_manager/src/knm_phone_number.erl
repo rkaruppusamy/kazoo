b8acbf3ef74e3622846bca6e58f0a8bb1e06b37a
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-25 15:36:02 +0200

    KAZOO-4980: fix compilation for test mode

--- core/kazoo_number_manager/src/knm_phone_number.erl
+++ core/kazoo_number_manager/src/knm_phone_number.erl
@@ -47,7 +47,9 @@
         ]).
 
 -export([list_attachments/2]).
+-ifndef(TEST).
 -export([push_stored/0]).
+-endif.
 
 -include("knm.hrl").
 -include_lib("kazoo/src/kz_json.hrl").
@@ -1004,6 +1006,7 @@ maybe_remove_number_from_account(Number) ->
             end
     end.
 
+-ifndef(TEST).
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -1062,3 +1065,4 @@ push_stored(Db) ->
         andalso lager:debug("save_docs ~p failed: ~p",
                             [[kz_doc:id(Doc) || Doc <- Docs], element(2,R)]),
     R.
+-endif.
