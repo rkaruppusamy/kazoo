b2e82637575108b9e9cb3efbc6d9f11c14525b19
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-25 16:03:40 +0200

    KAZOO-4980: log when de-aggregating db writes

--- core/kazoo_number_manager/src/knm_phone_number.erl
+++ core/kazoo_number_manager/src/knm_phone_number.erl
@@ -1049,6 +1049,7 @@ store_maybe_push(Db, Doc) ->
 %%--------------------------------------------------------------------
 -spec push_stored() -> 'ok'.
 push_stored() ->
+    lager:debug("pushing stored writes"),
     DBs = [Db || {Db=?NE_BINARY, I} <- get(),
                  is_integer(I)
           ],
